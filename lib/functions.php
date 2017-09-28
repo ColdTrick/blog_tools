97c044d184d56a8579b28d52204b0f60925a3519
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-09-28 17:56:27 +0200

    removed: unused function blog_tools_remove_blog_icon

--- lib/functions.php
+++ lib/functions.php
@@ -5,45 +5,6 @@
  */
 
 /**
- * Remove the icon of a blog
- *
- * @param ElggBlog $blog The blog to remove the icon from
- *
- * @return bool
- */
-function blog_tools_remove_blog_icon(ElggBlog $blog) {
-	
-	if (!($blog instanceof ElggBlog)) {
-		return false;
-	}
-	
-	if (empty($blog->icontime)) {
-		// no icon
-		return true;
-	}
-	
-	$icon_sizes = elgg_get_icon_sizes('object', 'blog');
-	if (!empty($icon_sizes)) {
-		$fh = new ElggFile();
-		$fh->owner_guid = $blog->getOwnerGUID();
-		
-		$prefix = "blogs/{$blog->getGUID()}";
-		
-		foreach ($icon_sizes as $name => $info) {
-			$fh->setFilename("{$prefix}{$name}.jpg");
-			
-			if ($fh->exists()) {
-				$fh->delete();
-			}
-		}
-	}
-	
-	unset($blog->icontime);
-	
-	return true;
-}
-
-/**
  * Check a plugin setting for the allowed use of advanced publication options
  *
  * @return bool
