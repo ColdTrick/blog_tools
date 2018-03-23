31542e2a0390aa06f387e5a4a2d31ce3f4b243e4
Jerome Bakker <jeabakker@coldtrick.com>
2018-03-23 09:29:39 +0100

    fixed: river layout

--- views/default/river/object/blog/create.php
+++ views/default/river/object/blog/create.php
@@ -9,8 +9,10 @@ $excerpt = $object->excerpt ? $object->excerpt : $object->description;
 $excerpt = elgg_get_excerpt($excerpt);
 
 $message = $excerpt;
-if (!empty($object->icontime)) {
-	$icon = elgg_view_entity_icon($object, 'small');
+if ($object->hasIcon('small')) {
+	$icon = elgg_view_entity_icon($object, 'small', [
+		'icon_wrapper' => true,
+	]);
 	
 	$message = elgg_format_element('div', ['class' => 'blog-tools-river-item clearfix'], $icon . $excerpt);
 }
