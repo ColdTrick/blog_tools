50ec3f494c01514fd860c8eeb8c5b7bc3995f1d1
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-12-14 13:52:57 +0100

    chore: updated translations

--- languages/nl.php
+++ languages/nl.php
@@ -1,19 +1,26 @@
 <?php
 /**
- * This file was created by Translation Editor v5.2
- * On 2016-11-01 15:06
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 13:40
  */
 
 return array (
-  'admin:upgrades:blog_tools_move_icons' => 'Verhuis blog iconen',
-  'admin:upgrades:blog_tools_move_icons:description' => 'De blog iconen moeten worden opgeslagen op een nieuwe locatie, deze upgrade zal alle huidige iconen verhuizen naar de nieuwe locatie. Als de blogs geen iconen hebben, voer dan deze upgrade uit en de iconen keren terug.',
+  'blog_tools:settings:menu' => 'Menu instellingen',
+  'blog_tools:settings:featured_menu' => 'Voeg een aangeraden tab toe aan blog lijst pagina\'s',
+  'blog_tools:force_notification' => 'Stuur een publicatie notificatie naar alle gebruikers wanneer deze blog is gepubliceerd',
+  'blog_tools:settings:featured_menu:help' => 'Toont een lijst met alle aangeraden blogs',
+  'blog_tools:settings:archive_menu' => 'Verplaats het blog archief menu naar een tab',
+  'blog_tools:settings:archive_menu:help' => 'Op blog lijst pagina\'s waar een archief wordt getoond in de zijbalk kan, middels deze instelling, dit menu verplaats worden naar een tab.',
+  'blog_tools:settings:full:show_full_related' => 'Toon gerelateerde blogs in de zijbalk',
+  'blog_tools:settings:force_notification' => 'Forceer notificaties voor nieuwe blogs',
+  'blog_tools:settings:force_notification:help' => 'Deze instelling geeft blog auteurs de mogelijkheid om geforceerd alle gebruikers op de hoogte te brengen indien een nieuwe blog wordt gepubliceerd. De notificatie voorkeuren van de ontvangende gebruikers wordt hierin wel gerespecteerd. Deze actie is enkel mogelijk voor persoonlijke blogs.',
+  'widgets:index_blog:name' => 'Blog berichten',
+  'widgets:index_blog:description' => 'Toon de laatste blogs',
   'blog_tools:view:related' => 'Gerelateerde blogs',
   'blog_tools:label:show_owner' => 'Toon informatie over jezelf onder het blog',
   'blog_tools:settings:full' => 'Blog detail weergave instellingen',
-  'blog_tools:settings:full:show_full_navigation' => 'Toon vorige/volgende navigatie',
   'blog_tools:settings:full:show_full_owner' => 'Toon blog eigenaars informatie onder de blog',
   'blog_tools:settings:full:show_full_owner:optional' => 'Optioneel, blog eigenaar mag dit bepalen',
-  'blog_tools:settings:size:master' => 'Master (550x550)',
   'blog_tools:settings:advanced_publication' => 'Maak geavanceerde publicatie opties beschikbaar',
   'blog_tools:settings:advanced_publication:description' => 'Als dit is ingeschakeld kunnen gebruikers een publicatie- en vervaldatum kiezen voor blogs. Dit vereist een werkende dagelijkse CRON.',
   'blog_tools:notify:publish:subject' => 'Een blog is gepubliceerd',
@@ -42,9 +49,6 @@ Je kun je blog hier bekijken:
   'blog_tools' => 'Blog tools',
   'blog_tools:toggle:feature' => 'Aanraden',
   'blog_tools:widget:featured' => 'Toon alleen aangeraden blogs?',
-  'blog_tools:label:icon:exists' => 'Upload een icoon (laat leeg om het huidige icoon te behouden)',
-  'blog_tools:label:icon:new' => 'Upload een icoon',
-  'blog_tools:label:icon:remove' => 'Verwijder icoon',
   'blog_tools:settings:image' => 'Blog afbeelding instellingen',
   'blog_tools:settings:other' => 'Andere instellingen',
   'blog_tools:settings:listing:image_align' => 'Blog lijstweergave afbeelding uitlijning',
@@ -52,16 +56,9 @@ Je kun je blog hier bekijken:
   'blog_tools:settings:full:image_align' => 'Blog detail weergave afbeelding uitlijning',
   'blog_tools:settings:full:image_size' => 'Blog detail weergave afbeelding grote',
   'blog_tools:settings:align:none' => 'Geen afbeelding',
-  'blog_tools:settings:size:tiny' => 'Erg klein (16x16)',
-  'blog_tools:settings:size:small' => 'Klein (40x40)',
-  'blog_tools:settings:size:medium' => 'Medium (100x100)',
-  'blog_tools:settings:size:large' => 'Groot (200x200)',
   'blog_tools:settings:listing:strapline' => 'Toon extra informatie in lijstweergave',
   'blog_tools:settings:strapline:default' => 'Standaard (eigenaar en tags)',
   'blog_tools:settings:strapline:time' => 'Alleen tijd',
-  'blog_tools:action:toggle_featured:error' => 'Er is een onbekende fout opgetreden bij het bewerken van de entity, probeer het nogmaals',
-  'blog_tools:action:toggle_featured:success' => 'De entity is succesvol bijgewerkt',
-  'blog_tools:widgets:index_blog:description' => 'Toon de laatste blogs op je community',
   'blog_tools:widgets:index_blog:view_mode' => 'Hoe moeten de blogs worden weergegeven',
   'blog_tools:widgets:index_blog:view_mode:list' => 'Lijst',
   'blog_tools:widgets:index_blog:view_mode:preview' => 'Voorbeeld',
