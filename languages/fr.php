7a7b03db4e5f8890a75aedbc09fc1251629a3fa0
Jerôme Bakker <jeabakker@coldtrick.com>
2021-02-11 09:00:26 +0100

    added: French translation
    for v 8.2.1
    
    Co-authored-by: Dan d'Auge <76149200+danhype@users.noreply.github.com>

--- languages/fr.php
+++ languages/fr.php
@@ -1,10 +1,29 @@
 <?php
-/**
- * This file was created by Translation Editor v7.0-beta
- * On 2018-12-14 13:49
- */
 
 return array (
+  'collection:object:blog:tag' => 'Blogs avec le tag : %s',
+  'blog_tools:label:publication_time' => 'Heure de publication',
+  'blog_tools:publication_time:description' => 'En combinaison avec la date de publication, cela fixera le moment de la publication.',
+  'blog_tools:force_notification' => 'Envoyer une notification de publication à tous les utilisateurs lorsque le blog est publié',
+  'blog_tools:edit:access:help' => 'Veuillez sélectionner le niveau d\'accès du blog lorsque le statut est défini sur "publié".',
+  'blog_tools:edit:access:help:publication' => 'Lorsque vous utilisez une date/heure de publication, le niveau d\'accès sera réglé sur privé jusqu\'à la date de publication. Lors de la publication, le niveau d\'accès sélectionné sera utilisé.',
+  'blog_tools:imprint:publication' => 'Publication prévue : %s',
+  'blog_tools:settings:menu' => 'Configuration du menu',
+  'blog_tools:settings:featured_menu' => 'Ajouter l\'onglet " En vedette " à la barre des onglets',
+  'blog_tools:settings:featured_menu:help' => 'Pour afficher une liste de tous les blogs mis en vedette',
+  'blog_tools:settings:archive_menu' => 'Déplacer de la barre latérale les archives de blog vers le menu de filtrage',
+  'blog_tools:settings:archive_menu:help' => 'Sur les pages listant les blogs, un onglet archive est affiché dans la barre latérale. Avec ce réglage, les archive peuvent être déplacées vers les onglets du menu de filtre.',
+  'blog_tools:settings:full:show_full_related' => 'Afficher les blogs en rapport dans la barre latérale',
+  'blog_tools:settings:advanced_publication' => 'Permettre les options de publication avancées',
+  'blog_tools:settings:advanced_publication:description' => 'Les utilisateurs peuvent ainsi choisir une date de publication pour les blogs. Nécessite un CRON fonctionnel.',
+  'blog_tools:settings:force_notification' => 'Forcer les notifications pour les nouveaux blogs',
+  'blog_tools:settings:force_notification:help' => 'Donne aux créateurs de blogs la possibilité de notifier tous les utilisateurs lorsqu\'un nouveau blog est créé. Les préférences de notification des utilisateurs destinataires seront toujours respectées. Cette fonction n\'est disponible que pour les blogs personnels.',
+  'blog_tools:action:toggle_feature:success:feature' => 'Le blog a été mis en vedette',
+  'blog_tools:action:toggle_feature:success:unfeature' => 'Le blog n\'est plus en vedette',
+  'blog_tools:action:toggle_feature:error:feature' => 'Une erreur s\'est produite lors de la mise en vedette du blog',
+  'blog_tools:action:toggle_feature:error:unfeature' => 'Une erreur s\'est produite lors du retrait du blog dans les blogs mis en vedette',
+  'widgets:index_blog:name' => 'Articles de blog',
+  'widgets:index_blog:description' => 'Afficher les derniers blogs sur le site',
   'blog_tools' => 'Outils pour les blogues',
   'blog_tools:toggle:feature' => 'En vedette',
   'blog_tools:toggle:unfeature' => 'Retirer de la section «En vedette »',
@@ -14,13 +33,6 @@ return array (
   'blog_tools:notify:publish:message' => 'Bonjour,
 
 votre blogue « %s » a été publié.
-
-Vous pouvez le visionner ici:
-%s',
-  'blog_tools:notify:expire:subject' => 'Un blogue est expirer',
-  'blog_tools:notify:expire:message' => 'Bonjour,
-
-votre blogue « %s » est expirer et n\'est plus visible par la communauté.
 
 Vous pouvez le visionner ici:
 %s',
