49cf317cfce1437951c6ffc566c9baa07c80fe88
Jerôme Bakker <jeabakker@coldtrick.com>
2023-07-26 15:05:06 +0200

    chore: wrapping up v12.0.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -4,7 +4,7 @@ use ColdTrick\BlogTools\Upgrades\MoveHeaderIcons;
 
 return [
 	'plugin' => [
-		'version' => '12.0',
+		'version' => '12.0.1',
 		'dependencies' => [
 			'blog' => [
 				'position' => 'after',
